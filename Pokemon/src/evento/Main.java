bcb1f1fc867b35d5083224ef1bdd448eb45aade2
mac0321 40 <mac0321.40@hulk.cec.ime.usp.br>
2015-05-11 08:35:24 -0300

    Deletando arquivos antigos

--- Pokemon/src/evento/Main.java
+++
@@ -1,9 +0,0 @@
-package evento;
-
-import Modeladores.Pokemons;
-
-public class Main {
-	public static void main(String[] args) {
-		Treinador player1 = new Treinador("player1",Pokemons.charizard,Pokemons.ninetales)
-	}
-}
